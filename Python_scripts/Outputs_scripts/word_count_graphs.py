d83bca466e4c5c1d4352df89d6c952f8f168247a
emfoxharp-ui <emfoxharp@gmail.com>
2026-04-16 21:30:06 +0100

    restructuring word count outputs

--- Python_scripts/Outputs_scripts/word_count_graphs.py
+++ Python_scripts/Outputs_scripts/word_count_graphs.py
@@ -33,26 +33,24 @@ word_count_df['total count'] = word_count
 word_count_df['unique count'] = unique_word_count
         
 #We can now maka a scattergraph using this information
-fig = plt.figure()
-ax = fig.add_subplot()
-plt.scatter(word_count,unique_word_count, color = '#5dde5b', s = 100)
+fig, axs = plt.subplots(1,2)
+ax = fig.add_subplot(axs[0])
+axs[0].scatter(word_count,unique_word_count, color = '#5dde5b', s = 100)
 #label each point (Kanye West needs different positioning so not to cause overlap)
 for i, txt in enumerate(word_count_df[0]):
     if txt == 'Kanye West':
-        ax.annotate(txt, (word_count[i] -45,unique_word_count[i]+0.8), fontweight = 'bold')
+        ax.annotate(txt, (word_count[i] -100,unique_word_count[i]+0.8), fontweight = 'bold')
     else:
         ax.annotate(txt, (word_count[i] +0.8,unique_word_count[i]+0.8), fontweight = 'bold')
 
 #add title and axis labels
-plt.title('Total Word Count and Unique Word Count', fontsize = 14, fontweight = 'bold')
-plt.xticks(np.arange(250,800, 25))
-plt.yticks(np.arange(50,350,25))
-plt.xlabel('Total Word Count', fontweight = 'bold')
-plt.ylabel('Unique Word Count', fontweight = 'bold')
-plt.grid(linestyle = '--', alpha = 0.5)
-
-
-plt.show()
+axs[0].set_title('Total Word Count and Unique Word Count', fontsize = 14, fontweight = 'bold')
+axs[0].set_xticks(np.arange(250,800, 25))
+axs[0].set_yticks(np.arange(50,350,25))
+axs[0].set_xlabel('Total Word Count', fontweight = 'bold')
+axs[0].set_ylabel('Unique Word Count', fontweight = 'bold')
+axs[0].grid(linestyle = '--', alpha = 0.5)
+axs[0].set_xticklabels(labels =np.arange(250,800, 25), rotation = 'vertical')
 
 
 #next make a bar chart showing for each artist the percentage of words they used that were unique
@@ -64,9 +62,10 @@ for i,txt in enumerate(word_count_df[0]):
 word_count_df['percentage'] = percentage
 word_count_df.sort_values(by= 'percentage', ascending = False, inplace = True)
 
-plt.bar(word_count_df[0], word_count_df['percentage'], color = '#4c97d9')
-plt.title('Unique words as a Percentage of Total Words', fontsize = 14, fontweight = 'bold')
-plt.xlabel('Artist', fontweight = 'bold')
-plt.ylabel('Percentage of Words That Are Unique', fontweight = 'bold')
-plt.grid(linestyle = '--', alpha = 0.5, axis = 'y')
+axs[1].bar(word_count_df[0], word_count_df['percentage'], color = '#4c97d9')
+axs[1].set_title('Unique words as a Percentage of Total Words', fontsize = 14, fontweight = 'bold')
+axs[1].set_xlabel('Artist', fontweight = 'bold')
+axs[1].set_ylabel('Percentage of Words That Are Unique', fontweight = 'bold')
+axs[1].grid(linestyle = '--', alpha = 0.5, axis = 'y')
+axs[1].set_xticklabels(word_count_df[0],rotation = 'vertical')
 plt.show()
